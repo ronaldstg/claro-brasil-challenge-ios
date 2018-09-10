394497c872b1352850e8651dc10b06d289eef84d
Ronald de Souza <ronald.souza.stg@gmail.com>
2018-09-10 16:27:39 -0300

    Adding icons, and some UI improvements

--- ClaroMovies/Controller/HomeViewController.swift
+++ ClaroMovies/Controller/HomeViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    let disposeBag = DisposeBag()
+    let cellIdentifier = "moviecell"
+    private let searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.placeholder = "Search for movies"
+        return searchController
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        navigationItem.searchController = searchController
+        navigationItem.title = "Claro Movies"
+        navigationItem.hidesSearchBarWhenScrolling = false
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
 
     override func didReceiveMemoryWarning() {
